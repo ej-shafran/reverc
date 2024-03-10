1e54584a3a357423b64976e8dd778c3c3defd46e
ej-shafran <ejshafran@hilma.tech>
2024-03-10 21:51:32 +0200

    Setup initial board

--- src/main.c
+++ src/main.c
@@ -8,6 +8,50 @@
 #define CELL_HEIGHT_PX 50
 #define CELL_PADDING_PX 10
 
+#define SCREEN_PADDING_X                                                   \
+	(int)((SCREEN_WIDTH -                                              \
+	       (OTHELLO_BOARD_SIZE * (CELL_WIDTH_PX + CELL_PADDING_PX))) / \
+	      2)
+#define SCREEN_PADDING_Y                                                    \
+	(int)((SCREEN_HEIGHT -                                              \
+	       (OTHELLO_BOARD_SIZE * (CELL_HEIGHT_PX + CELL_PADDING_PX))) / \
+	      2)
+
+void DrawCell(OthelloContext *c, int x, int y)
+{
+	Rectangle rec = {
+		.x = x * (CELL_WIDTH_PX + CELL_PADDING_PX) + SCREEN_PADDING_X,
+		.y = y * (CELL_HEIGHT_PX + CELL_PADDING_PX) + SCREEN_PADDING_Y,
+		.width = CELL_WIDTH_PX,
+		.height = CELL_HEIGHT_PX,
+	};
+
+	bool isEmpty = c->board[x][y] == OTHELLO_CELL_EMPTY;
+	bool isClicked = IsMouseButtonReleased(MOUSE_BUTTON_LEFT) &&
+			 CheckCollisionPointRec(GetMousePosition(), rec);
+
+	if (isEmpty && isClicked) {
+		c->board[x][y] = c->isBlack ? OTHELLO_CELL_BLACK :
+					      OTHELLO_CELL_WHITE;
+		c->isBlack = !c->isBlack;
+	}
+
+	Color color;
+	switch (c->board[x][y]) {
+	case OTHELLO_CELL_WHITE:
+		color = WHITE;
+		break;
+	case OTHELLO_CELL_BLACK:
+		color = BLACK;
+		break;
+	default:
+		color = GRAY;
+		break;
+	}
+
+	DrawRectangleRec(rec, color);
+}
+
 int main(void)
 {
 	OthelloContext c = {
@@ -15,72 +59,31 @@ int main(void)
 		.isBlack = true,
 	};
 
+	int first = (OTHELLO_BOARD_SIZE / 2) - 1;
+	int second = first + 1;
+
+	c.board[first][first] = OTHELLO_CELL_BLACK;
+	c.board[second][second] = OTHELLO_CELL_BLACK;
+	c.board[first][second] = OTHELLO_CELL_WHITE;
+	c.board[second][first] = OTHELLO_CELL_WHITE;
+
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "reverc");
 
 	while (!WindowShouldClose()) {
 		if (IsKeyReleased(KEY_Q))
-			goto EXIT;
+			break;
 
 		BeginDrawing();
 		ClearBackground(RED);
 
-		int screenPaddingX =
-			(SCREEN_WIDTH - (OTHELLO_BOARD_SIZE *
-					 (CELL_WIDTH_PX + CELL_PADDING_PX))) /
-			2;
-		int screenPaddingY =
-			(SCREEN_HEIGHT - (OTHELLO_BOARD_SIZE *
-					  (CELL_HEIGHT_PX + CELL_PADDING_PX))) /
-			2;
-
 		for (int x = 0; x < OTHELLO_BOARD_SIZE; ++x) {
 			for (int y = 0; y < OTHELLO_BOARD_SIZE; ++y) {
-				Rectangle rec = {
-					.x = x * (CELL_WIDTH_PX +
-						  CELL_PADDING_PX) +
-					     screenPaddingX,
-					.y = y * (CELL_HEIGHT_PX +
-						  CELL_PADDING_PX) +
-					     screenPaddingY,
-					.width = CELL_WIDTH_PX,
-					.height = CELL_HEIGHT_PX,
-				};
-
-				bool isEmpty = c.board[x][y] ==
-					       OTHELLO_CELL_EMPTY;
-				bool isClicked =
-					IsMouseButtonReleased(
-						MOUSE_BUTTON_LEFT) &&
-					CheckCollisionPointRec(
-						GetMousePosition(), rec);
-
-				if (isEmpty && isClicked) {
-					c.board[x][y] =
-						c.isBlack ? OTHELLO_CELL_BLACK :
-							    OTHELLO_CELL_WHITE;
-					c.isBlack = !c.isBlack;
-				}
-
-				Color color;
-				switch (c.board[x][y]) {
-				case OTHELLO_CELL_WHITE:
-					color = WHITE;
-					break;
-				case OTHELLO_CELL_BLACK:
-					color = BLACK;
-					break;
-				default:
-					color = GRAY;
-					break;
-				}
-
-				DrawRectangleRec(rec, color);
+				DrawCell(&c, x, y);
 			}
 		}
 		EndDrawing();
 	}
 
-EXIT:
 	CloseWindow();
 
 	return 0;
