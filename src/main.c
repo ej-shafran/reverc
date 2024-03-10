b4ef6e95d2d3094348802e6e167e0b5cec0d6696
ej-shafran <ejshafran@hilma.tech>
2024-03-10 19:10:25 +0200

    Render out an othello board

--- src/main.c
+++ src/main.c
@@ -2,49 +2,72 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
-#define SCREEN_PADDING 20
-#define SCREEN_COLOR GetColor(0x22bb22)
 
-#define CELL_WIDTH 100
-#define CELL_HEIGHT 100
-#define CELL_PADDING 10
-#define CELL_COLOR GetColor(0xbb22bb)
+#define BOARD_WIDTH 9
+#define BOARD_HEIGHT 9
+
+#define CELL_WIDTH_PX 50
+#define CELL_HEIGHT_PX 50
+#define CELL_PADDING_PX 10
+
+typedef enum {
+	CELL_EMPTY = 0,
+	CELL_WHITE,
+	CELL_BLACK,
+} CellState;
 
 int main(void)
 {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "reverc");
 
+	CellState board[BOARD_WIDTH][BOARD_HEIGHT] = { 0 };
+
 	while (!WindowShouldClose()) {
-		BeginDrawing();
-
-		ClearBackground(SCREEN_COLOR);
-
-		Rectangle bounding_box = {
-			.x = SCREEN_PADDING,
-			.y = SCREEN_PADDING,
-			.width = SCREEN_WIDTH - (SCREEN_PADDING * 2),
-			.height = SCREEN_HEIGHT - (SCREEN_PADDING * 2),
-		};
-
-		for (int x = bounding_box.x; x < bounding_box.width;
-		     x += CELL_WIDTH + CELL_PADDING) {
-			for (int y = bounding_box.y; y < bounding_box.height;
-			     y += CELL_HEIGHT + CELL_PADDING) {
-				Rectangle rec = { .y = y,
-						  .x = x,
-						  .width = CELL_WIDTH,
-						  .height = CELL_HEIGHT };
-
-				DrawRectangleRec(rec, CELL_COLOR);
-			}
-		}
-
-		EndDrawing();
-
 		if (IsKeyReleased(KEY_Q))
 			break;
+
+		BeginDrawing();
+		ClearBackground(RED);
+
+		int screenPaddingX =
+			(SCREEN_WIDTH -
+			 (BOARD_WIDTH * (CELL_WIDTH_PX + CELL_PADDING_PX))) /
+			2;
+		int screenPaddingY =
+			(SCREEN_HEIGHT -
+			 (BOARD_HEIGHT * (CELL_HEIGHT_PX + CELL_PADDING_PX))) /
+			2;
+
+		for (int x = 0; x < BOARD_WIDTH; ++x) {
+			for (int y = 0; y < BOARD_HEIGHT; y++) {
+				Rectangle rec = {
+					.x = x * (CELL_WIDTH_PX +
+						  CELL_PADDING_PX) +
+					     screenPaddingX,
+					.y = y * (CELL_HEIGHT_PX +
+						  CELL_PADDING_PX) +
+					     screenPaddingY,
+					.width = CELL_WIDTH_PX,
+					.height = CELL_HEIGHT_PX,
+				};
+
+				Color color;
+				switch (board[x][y]) {
+				case CELL_WHITE:
+					color = WHITE;
+				case CELL_BLACK:
+					color = BLACK;
+				default:
+					color = GRAY;
+				}
+
+				DrawRectangleRec(rec, color);
+			}
+		}
+		EndDrawing();
 	}
 
 	CloseWindow();
+
 	return 0;
 }
